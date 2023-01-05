09181655dff043c4c47d01a2fa1135e0aed861c8
name <yr1@gmail.com>
2023-01-05 20:44:21 +0300

    Экзамен 3
    Signed-off-by: name <yr1@gmail.com>

---
+++ Экзамен 3/задачи к экзамену 3.py
@@ -0,0 +1,15 @@
+# Задача 2
+# def palindrom(s):
+#     iters = len(s)//2
+#     for i in range(iters):
+#         if s[i] != s[-i -1]:
+#             return False
+#     return True
+#
+# while True:
+#     s = input('Inter a polindrom: ')
+#     print(f'{s} is polindrom' if palindrom(s) else 'not a polindrom')
+
+# Задача 1
+
+
